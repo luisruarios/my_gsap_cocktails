f0a936e17aab23cbb418ca2b5c43f38f8044841b
Luis R <luisruarios@outlook.com>
2026-05-30 02:15:32 -0500

    add art and about sections

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,21 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 import React from 'react'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Cocktails from './components/Cocktails';
+import Contact from './components/About';
+import About from './components/About';
+import Art from './components/Art';
 
 const App = () => {
   return (
 <main>
     <Navbar />
     <Hero />
-    <div className="h-dvh bg-black">
-      
-    </div>
+    <Cocktails />
+    <About />
+    <Art />
+
+
   </main>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,14 @@
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
-import React from "react";
+import React, { useRef } from "react";
 import gsap from "gsap";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef();
+
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   useGSAP(() => {
     const heroSplit = new SplitText(".title", { type: "chars,words" });
     const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
@@ -26,7 +31,8 @@ const Hero = () => {
       delay: 1,
     });
 
-    gsap.timeline({
+    gsap
+      .timeline({
         scrollTrigger: {
           trigger: "#hero",
           start: "top top",
@@ -34,8 +40,30 @@ const Hero = () => {
           scrub: true,
         },
       })
-      .to('.right-leaf', { y: 200 }, 0)
-      .to('.left-leaf', { y: -200 }, 0);
+      .to(".right-leaf", { y: 200 }, 0)
+      .to(".left-leaf", { y: -200 }, 0);
+
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    //video animation timeline
+    //create the timeline with a default duration
+
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
   }, []);
 
   return (
@@ -75,6 +103,16 @@ const Hero = () => {
           </div>
         </div>
       </section>
+
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          src="/videos/output.mp4"
+          muted
+          playsInline
+          preload="auto"
+        />
+      </div>
     </>
   );
 };
